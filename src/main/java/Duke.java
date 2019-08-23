18a3ddb747d1a4fdb078bffd4d3d048b1b1ced66
fauzt <fauzan.a.y@gmail.com>
2019-08-23 13:37:26 +0800

    added echo and exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner input = new Scanner(System.in);
+        String output = "";
+
+        do{
+            output = input.nextLine();
+            if (!output.equals("bye")) {
+                System.out.println(output);
+            }
+            else{
+                break;
+            }
+        }while(!output.equals("bye"));
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
+
