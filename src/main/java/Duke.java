faeed99f849550a6fb9b05c9bd070ea3aaa6e0b5
fauzt <fauzan.a.y@gmail.com>
2019-08-23 14:58:33 +0800

    added task marking functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,26 +14,37 @@ public class Duke {
 
         Scanner input = new Scanner(System.in);
         String output = "";
-        String[] list = new String[100];
-        int x = 0;
-        int count = 0;
+        Task[] list = new Task[100];
+        int x = 0, count = 0;
 
-        do{
+
+        loop: do{
             output = input.nextLine();
-            if (output.equals("list")){
-                while(x < count) {
-                    System.out.println( (x+1) + ". " + list[x]);
-                    x++;
-                }
-                x = 0;
-            }
-            else if (!output.equals("bye")) {
-                System.out.println("added: " + output + "\n");
-                list[count] = output;
-                count++;
-            }
-            else{
-                break;
+            String[] tokens = output.split("\\s+");
+            switch(tokens[0]){
+                case "list":
+                    System.out.println("Here are the tasks in your list:");
+                    while(x < count) {
+                        System.out.println( (x+1) + ".[" + list[x].getStatus() + "] " + list[x].description);
+                        x++;
+                    }
+                    System.out.println("\n");
+                    x = 0;
+                    break;
+                case "bye":
+                    break loop;
+                case "done":
+                    int item = Integer.parseInt(tokens[1]) - 1;
+                    list[item].markAsDone();
+                    System.out.println("Nice! I've marked this task as done:\n" +
+                            "[" + list[item].getStatus() + "] " + list[item].description + "\n");
+                    break;
+                default:
+                    System.out.println("added: " + output + "\n");
+                    Task t = new Task(output);
+                    list[count] = t;
+                    count++;
+
             }
         }while(!output.equals("bye"));
 
